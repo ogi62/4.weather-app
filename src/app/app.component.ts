1bdcd6190cb3de8d72d59d65700579c554f9b202
Ogi62 <danguba962@gmail.com>
2022-07-18 20:53:31 +0200

    made weather service and weather module and manage to make method for fetching the data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherData } from './models/weather.model';
+import { WeatherService } from './services/weather.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = '4.weather-app';
+export class AppComponent implements OnInit {
+
+  weatherData?: WeatherData;
+  
+  constructor(private weatherService: WeatherService) {}
+
+  ngOnInit(): void {
+    this.weatherService.getWeatherData('Belgrade')
+    .subscribe({
+      next: (response) => {
+        this.weatherData = response
+        console.log(response);
+      }
+    })
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,13 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  weatherApiBaseUrl: 'https://community-open-weather-map.p.rapidapi.com/weather',
+  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue: 'community-open-weather-map.p.rapidapi.com',
+  XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue: '959ab1ba7dmshb9b7f55aa2ac655p17197bjsn1f7efe207b54'
+
 };
 
 /*
